477c7645d1e74fe5a7c9ca8cf19d1c232ef40b16
Evan Kepner <evan.kepner@gmail.com>
2015-06-28 10:05:48 -0500

    update resizer

--- image_resizer.py
+++ image_resizer.py
@@ -7,12 +7,20 @@ pip install Wand
 import glob
 from wand.image import Image
 from wand.display import display
-
+import numpy as np
 
 def YieldImageSizes(dir_path):
 	for fn in glob.glob(dir_path):
 		with Image(filename=fn) as img:
 			yield img.size
+
+def ResizeImages(dir_path, size):
+	for fn in glob.glob(dir_path):
+		with Image(filename=fn) as img:
+			#using sample since it is faster than resize
+			#similar function but does not provide filter and blur options
+			img.sample(size[0], size[1])
+
 
 if __name__ == "__main__":
 
@@ -20,3 +28,15 @@ if __name__ == "__main__":
 
 	sizeList = [i for i in YieldImageSizes(dir_path)]
 	print sizeList
+
+	x = np.array(sizeList)
+	img_mean = x.mean(axis=0).round(0).astype(int)
+
+	print img_mean
+
+	ResizeImages(dir_path, img_mean)
+
+	newSizes = [i for i in YieldImageSizes(dir_path)]
+	print newSizes
+
+
