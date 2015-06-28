41ab16637ab9ca663783f45edfa7d9d2685415c7
Evan Kepner <evan.kepner@gmail.com>
2015-06-28 17:05:13 -0500

    update with resize tests

--- image_resizer.py
+++ image_resizer.py
@@ -8,6 +8,7 @@ import glob
 from wand.image import Image
 from wand.display import display
 import numpy as np
+import os
 
 def YieldImageSizes(dir_path):
 	for fn in glob.glob(dir_path):
@@ -20,7 +21,24 @@ def ResizeImages(dir_path, size):
 			#using sample since it is faster than resize
 			#similar function but does not provide filter and blur options
 			img.sample(size[0], size[1])
+			img.save(filename="./resize/r_" + os.path.basename(fn))
 
+def LiquidResizeImages(dir_path, size):
+	# DOES NOT WORK WITH DEFAULT IMAGEMAGICK ON CENTOS
+	for fn in glob.glob(dir_path):
+		with Image(filename=fn) as img:
+			img.liquid_rescale(size[0], size[1])
+			img.save(filename="./liquid/l_" + os.path.basename(fn))
+
+def TransformImage(dir_path, size):
+	for fn in glob.glob(dir_path):
+		with Image(filename=fn) as img:
+			resize_str = str(size[0]) + 'x' + str(size[1]) + '>'
+
+			#if larger than the resize_str, fit within box, preserving aspect ratio
+			img.transform(resize=resize_str)
+			img.save(filename="./transform/t_" + os.path.basename(fn))
+		
 
 if __name__ == "__main__":
 
@@ -32,11 +50,6 @@ if __name__ == "__main__":
 	x = np.array(sizeList)
 	img_mean = x.mean(axis=0).round(0).astype(int)
 
-	print img_mean
-
 	ResizeImages(dir_path, img_mean)
-
-	newSizes = [i for i in YieldImageSizes(dir_path)]
-	print newSizes
-
-
+	#LiquidResizeImages(dir_path, img_mean)
+	TransformImage(dir_path, img_mean)
