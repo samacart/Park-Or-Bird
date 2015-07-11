20351af1a886ca45cacea3d00b6eb8baf3d95d50
EvanKepner <evan.kepner@gmail.com>
2015-07-11 18:16:28 -0400

    update with error log

--- create_spark_image_inputs.py
+++ create_spark_image_inputs.py
@@ -2,6 +2,7 @@ import glob
 from PIL import Image
 import numpy as np
 import os
+import sys
 
 '''
 BIRD = gpfs1
@@ -22,19 +23,24 @@ if __name__ == "__main__":
     dir_path = '/gpfs/gpfsfpo/Bird/*.jpg'
 
     for fn in glob.glob(path):
-        #get raw image and resize with aspect ratio
-        #average size determined by prior code analysis
-        raw_img = Image.open(fn).resize((200,100), PIL.Image.ANTIALIAS)
+        try:
+            #get raw image and resize with aspect ratio
+            #average size determined by prior code analysis
+            raw_img = Image.open(fn).resize((200,100), PIL.Image.ANTIALIAS)
 
-        #convert to gray scale
-        gs_img = raw_img.convert("L")
+            #convert to gray scale
+            gs_img = raw_img.convert("L")
 
-        #apply Gaussian blur to the images
-        raw_blur = raw_img.filter(ImageFilter.GaussianBlur(radius=2))
-        gs_blur = gs_img.filter(ImageFilter.GaussianBlur(radius=2))
+            #apply Gaussian blur to the images
+            raw_blur = raw_img.filter(ImageFilter.GaussianBlur(radius=2))
+            gs_blur = gs_img.filter(ImageFilter.GaussianBlur(radius=2))
 
-        #create output files for all variations
-        writeOutput('/gpfs/gpfsfpo/shared/bird_raw.txt', raw_img)
-        writeOutput('/gpfs/gpfsfpo/shared/bird_gs.txt', gs_img)
-        writeOutput('/gpfs/gpfsfpo/shared/bird_raw_blur.txt', raw_blur)
-        writeOutput('/gpfs/gpfsfpo/shared/bird_gs_blur.txt', gs_blur)
+            #create output files for all variations
+            writeOutput('/gpfs/gpfsfpo/shared/bird_raw.txt', raw_img)
+            writeOutput('/gpfs/gpfsfpo/shared/bird_gs.txt', gs_img)
+            writeOutput('/gpfs/gpfsfpo/shared/bird_raw_blur.txt', raw_blur)
+            writeOutput('/gpfs/gpfsfpo/shared/bird_gs_blur.txt', gs_blur)
+
+        except:
+            with open('/gpfs/gpfsfpo/shared/bird_errors.log','a') as err:
+                err.write(fn + "\t" + sys.exc_info())
