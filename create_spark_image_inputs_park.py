ea663ed154aa4be9afee35453fb68d182c2d7c57
EvanKepner <evan.kepner@gmail.com>
2015-07-11 18:41:44 -0400

    add separate park bird create spark for cluster

---
+++ create_spark_image_inputs_park.py
@@ -0,0 +1,46 @@
+import glob
+from PIL import Image
+import numpy as np
+import os
+import sys
+
+'''
+BIRD = gpfs1
+PARK = gpfs2
+OTHER = gpfs3
+'''
+
+def writeOutput(outFile, img):
+
+    #write the image as a flattened numpy array to text files for Spark consumption
+    arr = np.array(img).flatten()
+    output = ' '.join(map(str, arr))
+    with open(outFile, 'a') as f:
+        f.write(output + "\n")
+
+if __name__ == "__main__":
+
+    dir_path = '/gpfs/gpfsfpo/Park/*.jpg'
+
+    for fn in glob.glob(path):
+        try:
+            #get raw image and resize with aspect ratio
+            #average size determined by prior code analysis
+            raw_img = Image.open(fn).resize((478,398), PIL.Image.ANTIALIAS)
+
+            #convert to gray scale
+            gs_img = raw_img.convert("L")
+
+            #apply Gaussian blur to the images
+            raw_blur = raw_img.filter(ImageFilter.GaussianBlur(radius=2))
+            gs_blur = gs_img.filter(ImageFilter.GaussianBlur(radius=2))
+
+            #create output files for all variations
+            writeOutput('/gpfs/gpfsfpo/shared/park_raw.txt', raw_img)
+            writeOutput('/gpfs/gpfsfpo/shared/park_gs.txt', gs_img)
+            writeOutput('/gpfs/gpfsfpo/shared/park_raw_blur.txt', raw_blur)
+            writeOutput('/gpfs/gpfsfpo/shared/park_gs_blur.txt', gs_blur)
+
+        except:
+            with open('/gpfs/gpfsfpo/shared/park_errors.log','a') as err:
+                err.write(fn + "\t" + sys.exc_info())
